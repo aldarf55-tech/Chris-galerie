c755f490c19abe7587699f7e2f6cde2b04fd65c5
Mk <al.darf55@gmail.com>
2026-08-06 20:21:16 +0200

    fix: correction du chemin du logo dans le header et mise a jour de la navigation

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from '../navbar/Navbar.tsx';
 import styles from './Header.module.css';
 import type { ViewType } from '../../../App.tsx'; // Importez le type depuis App
+import logoImg from '../../../assets/images/logocarre.jpg';
 
 interface HeaderProps {
   onViewChange: (view: ViewType) => void;
@@ -11,7 +12,7 @@ export const Header = ({ onViewChange }: HeaderProps) => {
     <header className={styles.header}>
       {/* Cliquer sur le logo ramène à la Galerie (au lieu d'un rechargement de page complet) */}
       <div className={styles.headerlogo} onClick={() => onViewChange('gallery')}>
-        <img src="src/assets/images/logocarre.jpg" alt="Logo" />
+        <img src={logoImg} alt="Logo" />
       </div>
 
       {/* On transmet onViewChange à la Navbar pour gérer les clics "Accueil" et "À propos" */}
